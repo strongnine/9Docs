cd4fa9a3f9bfab1afb2840ed17acb3c23311ba8f
strongnine <strongnine@163.com>
2022-04-02 13:54:02 +0800

    version update

--- docs/make.jl
+++ docs/make.jl
@@ -9,10 +9,7 @@ makedocs(
             "图神经网络" => "machine_learning/GNN.md",
         ],
         "产品思维" => "product/product_manager.md",
-        "个人推荐" => Any[
-            "书籍推荐" => "library/book.md",
-            "课程推荐" => "library/recommend.md"
-        ],
+        "个人推荐" => "library/recommend.md",
         "Git 学习笔记" => "git/git_notebook.md", 
     ],
 )
