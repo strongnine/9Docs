b265ba9d482ecb56b2c13f68ac0504b428885799
strongnine <strongnine@163.com>
2025-08-09 15:35:05 +0800

    Restart project.
    Edit make.jl (Add some pages).

--- docs/make.jl
+++ docs/make.jl
@@ -2,28 +2,28 @@ using Documenter
 
 makedocs(
     sitename = "9Docs", 
-    # pages = [
-    #     "Home" => "index.md",
-    #     "机器视觉" => Any[
-    #         "对比度增强" => "CV/contrast.md",
-    #         "暗通道先验去雾" => "CV/dark_channel_prior.md",
-    #     ],
-    #     "人工智能" => Any[
-    #         "特征工程" => "AI/FE.md",
-    #         "机器学习" => "AI/ML.md",
-    #         "神经网络" => "AI/NN.md",
-    #         "卷积神经网络" => "AI/CNN.md",
-    #         "循环神经网络" => "AI/RNN.md",
-    #         "图神经网络" => "AI/GNN.md",
-    #         "生成对抗网络" => "AI/GAN.md",
-    #         "计算机视觉" => "AI/CV.md",
-    #         "自然语言处理" => "AI/NLP.md",
-    #         "Transformer" => "AI/Transformer.md",
-    #     ],
-    #     "其他" => Any[
-    #         "推荐书籍大全" => "books.md",
-    #     ]
-    # ],
+    pages = [
+        "Home" => "index.md",
+        "机器视觉" => Any[
+            "对比度增强" => "CV/contrast.md",
+            "暗通道先验去雾" => "CV/dark_channel_prior.md",
+        ],
+        # "人工智能" => Any[
+        #     "特征工程" => "AI/FE.md",
+        #     "机器学习" => "AI/ML.md",
+        #     "神经网络" => "AI/NN.md",
+        #     "卷积神经网络" => "AI/CNN.md",
+        #     "循环神经网络" => "AI/RNN.md",
+        #     "图神经网络" => "AI/GNN.md",
+        #     "生成对抗网络" => "AI/GAN.md",
+        #     "计算机视觉" => "AI/CV.md",
+        #     "自然语言处理" => "AI/NLP.md",
+        #     "Transformer" => "AI/Transformer.md",
+        # ],
+        # "其他" => Any[
+        #     "推荐书籍大全" => "books.md",
+        # ]
+    ],
 )
 
 
